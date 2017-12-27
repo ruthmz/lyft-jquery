4d5e7e91d313820e30773ea1293b71ce0957c428
ruthmz <rmatosfer@gmail.com>
2017-12-27 02:29:47 -0500

    first commit

---
+++ js/signup.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  
+  $('#peru').click(function(){
+    $('.btn-style img') = $('#peru img');
+    $('.input-box').val('51');
+  })
+  $('#states').select(function(){
+    $('.input-box').val('51')
+  })
+
+});
