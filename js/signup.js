5e82399aae5e1b216ca2c24b4cb4f7a6c032c804
ruthmz <rmatosfer@gmail.com>
2017-12-27 21:36:16 -0500

    agregando jquery

--- js/signup.js
+++ js/signup.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-  
-  $('#peru').click(function(){
-    $('.btn-style img') = $('#peru img');
-    $('.input-box').val('51');
+  $('#mexico').click(function(){
+    $('#changeicon').attr('src','../assets/icons/mexico.ico');
+    $('.input-box').val('53')
   })
-  $('#states').select(function(){
+  
+  $('#venezuela').click(function(){
+    $('#changeicon').attr('src','../assets/icons/venezuela.ico');
+    $('.input-box').val('53')
+  })
+  $('#peru').click(function(){
+    $('#changeicon').attr('src','../assets/icons/peru.ico');
     $('.input-box').val('51')
   })
+  $('#states').click(function(){
+    $('#changeicon').attr('src','../assets/icons/states.ico')
+  })
+
+
+
 
 });
+
+// $('#states').click(function(){
+//   $('.input-box').val('81')
+// })
